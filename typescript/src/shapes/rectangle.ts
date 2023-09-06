93c71165d7b4cacecedc2bb8d57b9a0c04a73a1e
Zelin Wang <zelinewang@berkeley.com>
2023-09-06 14:46:44 -0400

    hide the width and height in Rectangle

--- typescript/src/shapes/rectangle.ts
+++ typescript/src/shapes/rectangle.ts
@@ -1,14 +1,9 @@
 interface Rectangle {
-    width: number,
-    height: number,
     computeArea: () => number
 }
 
 function newRectangle(width: number, height: number): Rectangle {
     return {
-        width,
-        height,
-
         computeArea: function (): number {
             return width * height
         }
